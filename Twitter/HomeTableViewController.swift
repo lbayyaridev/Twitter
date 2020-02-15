3ab301d42373437c653f0e2e571bf55fe45d2b6f
Laith <lbayyari@ucsd.edu>
2020-02-14 17:17:46 -0800

    Doneish

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -62,7 +62,7 @@ class HomeTableViewController: UITableViewController {
         numberOfTweet = numberOfTweet + 20
         let myParams = ["count": numberOfTweet]
         
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success:
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams as [String : Any], success:
             { (tweets: [NSDictionary]) in
                 
             self.tweetArray.removeAll()
@@ -103,6 +103,10 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["favorited"] as! Bool)
         
         return cell
     }
